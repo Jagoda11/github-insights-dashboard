c0ed2cad50ee1afa0495bf57c6d01420308f03d0
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-12-22 00:15:55 +0100

    Clean up for deployment

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// Define a simple function
-export function greet(name: string): void {
-  // eslint-disable-next-line no-console
-  console.log(`Hello, ${name}! 👋 Welcome to my project.`)
-}
-// Call the function
-greet('User')
