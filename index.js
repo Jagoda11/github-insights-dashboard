09027b349020c5ae852d04b2fe655cbeb51e77b6
Jagoda11 <Jagoda11@users.noreply.github.com>
2024-12-22 01:20:51 +0000

    deploy: 68c0e91273b5876c9118b4d6302c57100f63296a

--- index.js
+++
@@ -1,7 +0,0 @@
-// Define a simple function
-export function greet(name) {
-    // eslint-disable-next-line no-console
-    console.log(`Hello, ${name}! 👋 Welcome to my project.`);
-}
-// Call the function
-greet('User');
